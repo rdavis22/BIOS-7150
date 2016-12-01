712377a9ecc2cfe30e1413ba9e66cf38ef739887
Richard Davis <rdavis22@tulane.edu>
2016-12-01 06:10:17 +0000

    added updated predicated log odds plot

--- Homework/hw3/BIOS_7150_hw3_p2.R
+++ Homework/hw3/BIOS_7150_hw3_p2.R
@@ -24,7 +24,8 @@ hw3_p2.model<-glm(mg~num_part+trich+age+num_part*trich,
 
 #transform age into an interval predictor and see if it fits
 contrasts(age)<-contr.poly(3)
-cont_age<-as.factor(age)
+ord.age<-factor(mg.data$age,
+                levels = c("1", "2", "3"), ordered = T)
 # attr(,"contrasts")
 # .L         .Q
 # 1 -7.071068e-01  0.4082483
@@ -33,52 +34,57 @@ cont_age<-as.factor(age)
 # Levels: 1 2 3
 
 #get the model with interval coded covariate for bmi
-hw3_p2_ord.model<-glm(mg~num_part+trich+cont_age+num_part*trich,
+hw3_p2_ord.model<-glm(mg~num_part+trich+ord.age+num_part*trich,
                       data=mg.data, weights = Count, family=binomial)
 #get the saturated model for GoF comparion
-hw3_p2_sat.model<-glm(mg~num_part+trich+age+num_part*trich+num_part*age,
+hw3_p2_sat.model<-glm(mg~num_part+trich+age+num_part*trich+num_part*age+trich*age,
                       weights=Count, data=mg.data, family=binomial)
 
 #get odds ratios
 #create an interaction term column for the data
 mg.data$num_part.trich<-interaction(mg.data$num_part, mg.data$trich)
 #run the model again with the interaction column predictor
-intrct.model<-glm(mg~cont_age+num_part.trich, 
+intrct.model<-glm(mg~ord.age+num_part.trich, 
                   data=mg.data, weight=Count, family=binomial)
 
 #get the matrix of coefficients using the "cont_age" and interaction term
-K1<-glht(intrct.model, mcp(cont_age = "Tukey"))$linfct
+K1<-glht(intrct.model, mcp(ord.age = "Tukey"))$linfct #only does dummy coded
 K2<-glht(intrct.model, mcp(num_part.trich = "Tukey"))$linfct
 All_OR <- glht(intrct.model, linfct = rbind(K1, K2))
 All_OR_cint <-confint(All_OR)
 
 ##Plots of predicted log Odds##
 #get the predicted log odds (trich=0)
-lodds_model<-predict.glm(intrct.model)
-lodds_num0_part0<-lodds_model[seq(from=1, to=6, by=2)]
-lodds_num0_part1<-lodds_model[seq(from=7, to=12, by=2)]
-lodds_num1_part0<-lodds_model[seq(from=13, to=18, by=2)]
-lodds_num1_part1<-lodds_model[seq(from=19, to=24, by=2)]
-names(lodds_num0_part0)<-c("age1", "age2", "age3")
+# lodds_model<-predict.glm(intrct.model)
+# lodds_num0_part0<-lodds_model[seq(from=1, to=6, by=2)]
+# lodds_num0_part1<-lodds_model[seq(from=7, to=12, by=2)]
+# lodds_num1_part0<-lodds_model[seq(from=13, to=18, by=2)]
+# lodds_num1_part1<-lodds_model[seq(from=19, to=24, by=2)]
+lodds_num_trich_11<-c(-1.3881, -2.2474, -3.1067)
+lodds_num_trich_10<-c(-2.3959, -3.2552, -4.1145)
+lodds_num_trich_01<-c(-3.7312, -4.5905, -5.4498)
+lodds_num_trich_00<-c(-3.1535, -4.0128, -4.8721)
+names(lodds_num_trich_11)<-c("age1", "age2", "age3")
 
-num_part.data<-data.frame(lodds_num0_part0, lodds_num0_part1, lodds_num1_part0,
-                 lodds_num1_part1, names(lodds_num0_part0))
+num_part.data<-data.frame(lodds_num_trich_00, lodds_num_trich_01,
+                          lodds_num_trich_10, lodds_num_trich_11,
+                          names(lodds_num_trich_11))
 
 p_num_part<-ggplot(num_part.data)+
   #add ethnic 1, hdl log odds points to plot
-  geom_point(aes(x=names.lodds_num0_part0., y=lodds_num0_part0,
-                 colour="num0part0"), size=2.5)+
-  geom_point(aes(x=names.lodds_num0_part0., y=lodds_num0_part1,
-                 colour="num0part1"), size=2.5)+
-  geom_point(aes(x=names.lodds_num0_part0., y=lodds_num1_part0,
-                 colour="num1part0"), size=2.5)+
-  geom_point(aes(x=names.lodds_num0_part0., y=lodds_num1_part1,
-                 colour="num1part1"), size=2.5)+
+  geom_point(aes(x=names.lodds_num_trich_11., y=lodds_num_trich_00,
+                 colour="num0trich0"), size=2.5)+
+  geom_point(aes(x=names.lodds_num_trich_11., y=lodds_num_trich_01,
+                 colour="num0trich1"), size=2.5)+
+  geom_point(aes(x=names.lodds_num_trich_11., y=lodds_num_trich_10,
+                 colour="num1trich0"), size=2.5)+
+  geom_point(aes(x=names.lodds_num_trich_11., y=lodds_num_trich_11,
+                 colour="num1trich1"), size=2.5)+
 #add a connecting line between points to plot
-  geom_line(aes(x=names.lodds_num0_part0., y=lodds_num0_part0), group=1)+
-  geom_line(aes(x=names.lodds_num0_part0., y=lodds_num0_part1), group=2)+
-  geom_line(aes(x=names.lodds_num0_part0., y=lodds_num1_part0), group=3)+
-  geom_line(aes(x=names.lodds_num0_part0., y=lodds_num1_part1), group=4)+
+  geom_line(aes(x=names.lodds_num_trich_11., y=lodds_num_trich_00), group=1)+
+  geom_line(aes(x=names.lodds_num_trich_11., y=lodds_num_trich_01), group=2)+
+  geom_line(aes(x=names.lodds_num_trich_11., y=lodds_num_trich_10), group=3)+
+  geom_line(aes(x=names.lodds_num_trich_11., y=lodds_num_trich_11), group=4)+
   #add axis and main title
   labs(title="Log odds for Number of Part. and Trich vs. Age",
        x="Age", y="Predicted Log Odds")
