2f31b705caaad182c9e94d62e1baae0be32ffb29
Richard Davis <rdavis22@tulane.edu>
2016-12-04 15:17:32 +0000

    added unadjusted effects

--- Homework/hw4/BIOS_7150_hw4.R
+++ Homework/hw4/BIOS_7150_hw4.R
@@ -12,7 +12,6 @@ if(!require(MASS))
 
 #load the melanoma data
 #hw4.data<-read_csv(file=file.choose(), col_names=T)
-hw4.tibble<-as_tibble(hw4.data)
 
 
 #transform the categorical variables to class "factor"...
@@ -20,41 +19,65 @@ hw4.tibble<-as_tibble(hw4.data)
 #stratification variable #cannot be class factor!!! Otherwise, we will throw ...
 #an error: #"Cox model doesn't support 'mright' survival data"...
 #This issue is not present with the "clogistic" function from the "Epi" package
-hw4.tibble<-mutate_each(hw4.tibble, funs(factor), skin, hair, eyes,
+hw4.data<-mutate_each(hw4.data, funs(factor), skin, hair, eyes,
                         freckles, acute, chronic)
-attach(hw4.tibble)
+attach(hw4.data)
 
 #check for appropriateness of ordinal simplification in ordinal categorical...
 #variables "freckles", "acute", and "chronic"
-contrasts(hw4.tibble$freckles) <- contr.poly(3)
-ord.freckles<-factor(hw4.tibble$freckles,
+contrasts(hw4.data$freckles) <- contr.poly(3)
+ord.freckles<-factor(hw4.data$freckles,
                      levels = c("1", "2", "3"), ordered = T)
-contrasts(hw4.tibble$acute) <- contr.poly(4)
-ord.acute<-factor(hw4.tibble$acute,
+contrasts(hw4.data$acute) <- contr.poly(4)
+ord.acute<-factor(hw4.data$acute,
                   levels = c("1", "2", "3", "4"), ordered = T)
-contrasts(hw4.tibble$chronic) <- contr.poly(4)
-ord.chronic<-factor(hw4.tibble$chronic, 
+contrasts(hw4.data$chronic) <- contr.poly(4)
+ord.chronic<-factor(hw4.data$chronic, 
                     levels = c("1", "2", "3", "4"), ordered = T)
 #append new 
-hw4.tibble<-add_column(hw4.tibble, ord.freckles, ord.acute, ord.chronic)
+#hw4.data<-add_column(hw4.data, ord.freckles, ord.acute, ord.chronic)
 
 ####Model Selection####
 #Null model
+L_2.null<-length(hw4.data$casecon)*log(0.5)
 
-#Full Main effects model
-hw4.logit<-clogit(casecon~skin+hair+eyes+ord.freckles+ord.acute+ord.chronic+
+#get the unadjusted effects of each predictor
+skin.logit<-clogit(casecon~skin+strata(pair))
+hair.logit<-clogit(casecon~hair+strata(pair))
+eyes.logit<-clogit(casecon~eyes+strata(pair))
+ord.freckles.logit<-clogit(casecon~ord.freckles+strata(pair))
+ord.acute.logit<-clogit(casecon~ord.acute+strata(pair))
+ord.chronic.logit<-clogit(casecon~ord.chronic+strata(pair))
+nvsmall.logit<-clogit(casecon~ord.chronic+strata(pair))
+nvlarge.logit<-clogit(casecon~nvlarge+strata(pair))
+nvtot.logit<-clogit(casecon~nvtot+strata(pair))
+ant15.logit<-clogit(casecon~ant15+strata(pair))
+
+#Log-likelihoods of the unadjusted models
+L_2.skin<-logLik(skin.logit)
+L_2.hair<-logLik(hair.logit)
+L_2.eyes<-logLik(eyes.logit)
+L_2.ord.freckles<-logLik(ord.freckles.logit)
+L_2.ord.acute<-logLik(ord.acute.logit)
+L_2.ord.chronic<-logLik(ord.chronic.logit)
+L_2.nvsmall<-logLik(nvsmall.logit)
+L_2.nvlarge<-logLik(nvlarge.logit)
+L_2.nvtot<-logLik(nvtot.logit)
+L_2.ant15<-logLik(ant15.logit)
+
+##Full Main effects model
+fme.logit<-clogit(casecon~skin+hair+eyes+ord.freckles+ord.acute+ord.chronic+
                     nvsmall+nvlarge+nvtot+ant15+strata(pair), method="exact",
-                  data=hw4.tibble)
-
+                  data=hw4.data)
 #Best subset regression for the model
-hw4.glmulti<-glmulti(Surv(rep(1, 200L), casecon)~skin+hair+eyes+ord.freckles+
-                       ord.acute+ord.chronic+nvsmall+nvlarge+nvtot+ant15+strata(pair),
-                     data=hw4.tibble,
-                     level=2, method="h",
-                     crit="aic",
-                     confsetsize = 5,
-                     plotty=F, report=F,
-                     fitfunction = "coxph")
+# hw4.glmulti<-glmulti(Surv(rep(1, 200L), casecon)~skin+hair+eyes+ord.freckles+
+#                        ord.acute+ord.chronic+nvsmall+nvlarge+nvtot+ant15+strata(pair),
+#                      data=hw4.data,
+#                      level=2, method="h",
+#                      crit="aic",
+#                      confsetsize = 5,
+#                      plotty=F, report=F,
+#                      fitfunction = "coxph")
 
 #StepWise Regression for the model
 # hw4.step=stepAIC(hw4.logit, direction="both")
