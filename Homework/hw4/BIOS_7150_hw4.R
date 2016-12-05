bc936e6f2bfba469615d024808939c8b46ec0773
Richard Davis <rdavis22@tulane.edu>
2016-12-05 05:47:26 +0000

    added stepwise regression

--- Homework/hw4/BIOS_7150_hw4.R
+++ Homework/hw4/BIOS_7150_hw4.R
@@ -11,7 +11,7 @@ if(!require(MASS))
   install.packages("MASS")
 
 #load the melanoma data
-#hw4.data<-read_csv(file=file.choose(), col_names=T)
+hw4.data<-read_csv(file=file.choose(), col_names=T)
 
 
 #transform the categorical variables to class "factor"...
@@ -35,7 +35,7 @@ contrasts(hw4.data$chronic) <- contr.poly(4)
 ord.chronic<-factor(hw4.data$chronic, 
                     levels = c("1", "2", "3", "4"), ordered = T)
 #append new 
-#hw4.data<-add_column(hw4.data, ord.freckles, ord.acute, ord.chronic)
+hw4.data<-add_column(hw4.data, ord.freckles, ord.acute, ord.chronic)
 
 ####Model Selection####
 #Null model
@@ -69,15 +69,24 @@ L_2.ant15<-logLik(ant15.logit)
 fme.logit<-clogit(casecon~skin+hair+eyes+ord.freckles+ord.acute+ord.chronic+
                     nvsmall+nvlarge+nvtot+ant15+strata(pair), method="exact",
                   data=hw4.data)
+
+hw4.data<-mutate_each(hw4.data, funs(as.numeric), ord.freckles)
+
+#StepWise Regression for the model
+hw4.step=stepAIC(fme.logit, direction="both")
+
+#forward Regression for the model
+hw4.forward=stepAIC(fme.logit, direction="forward")
+
+#forward Regression for the model
+hw4.backward=stepAIC(fme.logit, direction="backward")
+
 #Best subset regression for the model
-# hw4.glmulti<-glmulti(Surv(rep(1, 200L), casecon)~skin+hair+eyes+ord.freckles+
-#                        ord.acute+ord.chronic+nvsmall+nvlarge+nvtot+ant15+strata(pair),
+# hw4.glmulti<-glmulti(Surv(rep(1, 200L), casecon)~skin+hair+eyes+
+#                        ord.chronic+nvsmall+nvlarge+nvtot+ant15+strata(pair),
 #                      data=hw4.data,
 #                      level=2, method="h",
 #                      crit="aic",
 #                      confsetsize = 5,
 #                      plotty=F, report=F,
 #                      fitfunction = "coxph")
-
-#StepWise Regression for the model
-# hw4.step=stepAIC(hw4.logit, direction="both")
