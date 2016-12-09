14803a05b73bd4c27c79a4a591a9468453fd7c70
Richard Davis <rdavis22@tulane.edu>
2016-12-09 15:02:53 +0000

    lec 10 script final

--- Lecture/Lecture_scripts/lec_10 script.R
+++ Lecture/Lecture_scripts/lec_10 script.R
@@ -10,15 +10,19 @@
 #   sacrificeflies = data.frame(value=sacrificeflies,teamID,yearID,runs),
 #   );
 # detach(team.batting.00to08)
+rm(list=ls())
 library(vcd)
-blarg<-data.frame(expand.grid(alcohol=c("No","Yes"),
-                              gender=c("male", "female"),
-                              ethnic=c("AA", "Hispanic", "White", "Other")
-                              ))
-blerg<-c(459, 280, 507, 306, 15, 16, 17, 26, 655, 761, 695, 783, 64, 59, 48, 47)
-acttsmoke<-cbind(blarg, blerg)
+acttsmoke<-data.frame
+#alcohol<-c("No","Yes")
+#gender<-c("male", "female")
+alcohol=c("No","Yes"),
+gender=c("male", "female"),
+ethnic<-c("AA", "Hispanic", "White", "Other"),
+actcount<-c(459, 280, 507, 306, 15, 16, 17, 26, 655, 761, 695, 783, 64, 59, 48, 47)
+#acttsmoke<-cbind(alcohol, actcount)
+attach(acttsmoke)
 hssmoke<-glm(alcohol~ethnic, family="binomial", data=acttsmoke)
                               #count=c(459, 15, 655, 64, 507, 17, 695, 48, 
                                       #280, 16, 761, 59, 306, 26, 783, 47)))
-                              
-                                
+v<-vcov(hssmoke)
+logLik(hssmoke)
